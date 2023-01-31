2b2c651730d73ddd42bd5868eb0daf75e3b6f132
atsushimachida <atsmcd1029@gmail.com>
2023-01-31 20:03:08 +0900

    Review01の再提出

--- src/Reviw01.java
+++ src/Reviw01.java
@@ -2,13 +2,12 @@
 public class Reviw01 {
 
     public static void main(String[] args) {
-        int tax = taxMethod (10);
         int price = 1500;
+        int tax = taxMethod (10,price);
         System.out.println("1500円の商品の税込価格は" + (price + tax)+ "円(消費税は" + tax + "円)です。");
         }
 
-    public static int taxMethod(int tax) {
-        int price =1500;
+    public static int taxMethod(int tax,int price) {
         int result =  price * tax/100;
         return result;
     }
